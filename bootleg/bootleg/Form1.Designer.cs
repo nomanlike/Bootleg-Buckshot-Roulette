ace571ea0f9e7781b2d127fc2fedeb191a990ef9
nomanlike <57402905+nomanlike@users.noreply.github.com>
2024-04-14 21:21:21 +0700

    aaa

---
+++ bootleg/bootleg/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace bootleg
+{
+    partial class Bootleg_game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Event_box = new System.Windows.Forms.TextBox();
+            this.Him = new System.Windows.Forms.Button();
+            this.Yourself = new System.Windows.Forms.Button();
+            this.Lives_of_yours = new System.Windows.Forms.TextBox();
+            this.Lives_of_his = new System.Windows.Forms.TextBox();
+            this.Retry = new System.Windows.Forms.Button();
+            this.end_turn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Event_box
+            // 
+            this.Event_box.Location = new System.Drawing.Point(221, 210);
+            this.Event_box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Event_box.Name = "Event_box";
+            this.Event_box.ReadOnly = true;
+            this.Event_box.Size = new System.Drawing.Size(269, 22);
+            this.Event_box.TabIndex = 0;
+            this.Event_box.Text = "Event shown";
+            this.Event_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Him
+            // 
+            this.Him.Location = new System.Drawing.Point(380, 127);
+            this.Him.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Him.Name = "Him";
+            this.Him.Size = new System.Drawing.Size(109, 42);
+            this.Him.TabIndex = 1;
+            this.Him.Text = "Shoot Him";
+            this.Him.UseVisualStyleBackColor = true;
+            this.Him.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Yourself
+            // 
+            this.Yourself.Location = new System.Drawing.Point(221, 273);
+            this.Yourself.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Yourself.Name = "Yourself";
+            this.Yourself.Size = new System.Drawing.Size(109, 42);
+            this.Yourself.TabIndex = 2;
+            this.Yourself.Text = "Shoot yourself";
+            this.Yourself.UseVisualStyleBackColor = true;
+            this.Yourself.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Lives_of_yours
+            // 
+            this.Lives_of_yours.Location = new System.Drawing.Point(221, 350);
+            this.Lives_of_yours.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Lives_of_yours.Name = "Lives_of_yours";
+            this.Lives_of_yours.ReadOnly = true;
+            this.Lives_of_yours.Size = new System.Drawing.Size(100, 22);
+            this.Lives_of_yours.TabIndex = 3;
+            this.Lives_of_yours.TextChanged += new System.EventHandler(this.Lives_of_yours_TextChanged);
+            // 
+            // Lives_of_his
+            // 
+            this.Lives_of_his.Location = new System.Drawing.Point(389, 74);
+            this.Lives_of_his.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Lives_of_his.Name = "Lives_of_his";
+            this.Lives_of_his.ReadOnly = true;
+            this.Lives_of_his.Size = new System.Drawing.Size(100, 22);
+            this.Lives_of_his.TabIndex = 4;
+            this.Lives_of_his.TextChanged += new System.EventHandler(this.Lives_of_his_TextChanged);
+            // 
+            // Retry
+            // 
+            this.Retry.Location = new System.Drawing.Point(641, 273);
+            this.Retry.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Retry.Name = "Retry";
+            this.Retry.Size = new System.Drawing.Size(75, 42);
+            this.Retry.TabIndex = 5;
+            this.Retry.Text = "Retry";
+            this.Retry.UseVisualStyleBackColor = true;
+            this.Retry.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // end_turn
+            // 
+            this.end_turn.Location = new System.Drawing.Point(392, 273);
+            this.end_turn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.end_turn.Name = "end_turn";
+            this.end_turn.Size = new System.Drawing.Size(100, 42);
+            this.end_turn.TabIndex = 6;
+            this.end_turn.Text = "End your turn";
+            this.end_turn.UseVisualStyleBackColor = true;
+            this.end_turn.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Bootleg_game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.end_turn);
+            this.Controls.Add(this.Retry);
+            this.Controls.Add(this.Lives_of_his);
+            this.Controls.Add(this.Lives_of_yours);
+            this.Controls.Add(this.Yourself);
+            this.Controls.Add(this.Him);
+            this.Controls.Add(this.Event_box);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Bootleg_game";
+            this.Text = "Bootleg Buckshot Roulette";
+            this.Load += new System.EventHandler(this.Bootleg_game_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Event_box;
+        private System.Windows.Forms.Button Him;
+        private System.Windows.Forms.Button Yourself;
+        private System.Windows.Forms.TextBox Lives_of_yours;
+        private System.Windows.Forms.TextBox Lives_of_his;
+        private System.Windows.Forms.Button Retry;
+        private System.Windows.Forms.Button end_turn;
+    }
+}
+
---
+++ bootleg/bootleg/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bootleg
+{
+    public partial class Bootleg_game : Form
+    {
+        List<String> shots=new List<String>();
+        int p_lives=4;
+        int d_lives=4;
+
+        int total;
+        int blank;
+        Boolean turn;
+        Boolean game;
+        public Bootleg_game()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //shoot yourself
+            String line;
+
+            
+
+            line = shots[0];
+            shots.RemoveAt(0);
+            if (game)
+            {
+                if (turn && p_lives > 0)
+                {
+                    Event_box.Text = "You shot yourself. It is a " + line;
+                    turn = false;
+                    if (line == "blank")
+                    {
+                        turn = true;
+                    }
+
+                    if (line == "live")
+                    {
+                        p_lives--;
+                        update_life();
+                    }
+
+                }
+                else
+                {
+                    Event_box.Text = "End your turn. NOW";
+                    if (p_lives == 0)
+                    {
+                        Event_box.Text = "Press retry to restart";
+                    }
+                }
+
+                if (shots.Count == 0)
+                {
+                    Load_shell();
+                }
+            }
+            else { Event_box.Text = "He is dead already. Press retry"; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //shoot him
+            String line;
+
+            
+            line = shots[0];
+            shots.RemoveAt(0);
+
+            if (game)
+            {
+                if (turn && p_lives > 0)
+                {
+                    Event_box.Text = "You shot him. It is a " + line;
+                    turn = false;
+                    if (line == "live")
+                    {
+                        d_lives--;
+                        update_life();
+                    }
+                }
+                else
+                {
+                    Event_box.Text = "End your turn. NOW";
+                    if (p_lives == 0)
+                    {
+                        Event_box.Text = "Press retry to restart";
+                    }
+                }
+
+                if (shots.Count == 0)
+                {
+                    Load_shell();
+                }
+            }
+            else
+            {
+                Event_box.Text = "He is dead already. Press retry";
+            }
+
+
+        }
+
+        private void Bootleg_game_Load(object sender, EventArgs e)
+        {
+            shots = new List<String>();
+            p_lives = 4;
+            d_lives = 4;
+            Lives_of_yours.Text = p_lives.ToString();
+            Lives_of_his.Text = d_lives.ToString();
+            game = true;
+            Load_shell();
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            shots = new List<String>();
+            p_lives = 4;
+            d_lives = 4;
+            Lives_of_yours.Text = p_lives.ToString();
+            Lives_of_his.Text = d_lives.ToString();
+            game = true;
+            Load_shell();
+
+
+        }
+
+        private void Lives_of_yours_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Lives_of_his_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if (game)
+            {
+                if (!turn && p_lives > 0)
+                {
+                    turn = true;
+                    //his turn
+                    String line;
+                    line = shots[0];
+                    shots.RemoveAt(0);
+
+                    double chance = (double)blank / total;
+                    chance = (chance * 100.00);
+
+
+                    if ((int)chance <= 50)
+                    {
+                        Event_box.Text = "He shot you. It is a " + line;
+                        if (line == "live")
+                        {
+                            p_lives--;
+                            update_life();
+                        }
+                    }
+
+                    if (chance > 50.00)
+                    {
+                        Event_box.Text = "He shot himself. It is a " + line;
+                        if (line == "live")
+                        {
+                            d_lives--;
+                            update_life();
+                        }
+                        if (line == "blank")
+                        {
+                            turn = false;
+                        }
+
+
+                    }
+
+
+
+
+
+                    if (shots.Count == 0)
+                    {
+                        Load_shell();
+                    }
+
+                }
+                else
+                {
+                    Event_box.Text = (total - blank).ToString() + " live " + blank.ToString() + " blank. Don't throw away your life";
+                    if (p_lives == 0)
+                    {
+                        Event_box.Text = "Game Over, pal. Press retry";
+                    }
+                }
+            }
+            else
+            { Event_box.Text = "He is dead already. Press retry"; }
+
+
+        }
+
+        private void update_life()
+        {
+            Lives_of_yours.Text = p_lives.ToString();
+            Lives_of_his.Text = d_lives.ToString();
+
+            if (p_lives == 0)
+            { Event_box.Text = "Game Over, pal. Press retry"; }
+            if (d_lives == 0)
+            { Event_box.Text = "He is dead already. Press retry";
+                game = false;
+            }
+        }
+
+        private void Load_shell()
+        {
+            turn = true;
+           
+            Random r_t=new Random();
+            Random r_b = new Random();
+            total = r_t.Next(2,8);
+            blank = r_b.Next(1,total-1);
+            
+
+
+            List<String> bar = new List<String>();
+
+            for (int i = 0; i < blank; i++)
+            {
+                bar.Add("blank");
+            }
+            for (int i = 0; i < total - blank; i++)
+            {
+                bar.Add("live");
+            }
+
+            Event_box.Text = (total-blank).ToString() + " live " + blank.ToString()+" blank";
+
+            int n;
+
+            while (bar.Count > 0)
+            {
+                n=r_t.Next(0,bar.Count()-1);
+                shots.Add(bar[n]);
+                bar.RemoveAt(n);
+            }
+            
+           
+
+        }
+
+       
+    }
+}
